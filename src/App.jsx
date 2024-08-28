4b6e1a365f1c35bda1d3d3585419a65042c95c9e
Ritiksingh101 <rs1982thakur@gmail.com>
2024-08-28 22:12:02 +0530

    Initial setup and Day 1 tasks completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import './index.css' // This is the correct way to import CSS
+
+function App() {
+  return (
+    <div className="text-center text-2xl text-blue-500">
+      Tailwind CSS is Working!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+// src/main.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // Import from 'react-dom/client'
+import App from './App';
+import './index.css';
+import DashboardProvider from './context/DashboardContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Use createRoot from ReactDOM
+root.render(
+  <React.StrictMode>
+    <DashboardProvider>
+    <App />
+    </DashboardProvider>
+     </React.StrictMode>
+);
